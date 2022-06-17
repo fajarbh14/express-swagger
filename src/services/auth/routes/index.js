903d807e4089d1236d00f657ae95a5ed518c5e8d
Deweks0 <lutfiahjr1029f@gmail.com>
2022-06-17 11:01:20 +0700

    [UPDATE] added middleware auth

--- src/services/auth/routes/index.js
+++ src/services/auth/routes/index.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const AuthController = require("../../auth/controllers/AuthController");
+const authMiddleware = require("../../../middlewares/AuthMiddleware");
 const router = Router();
 
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
-router.get("/create-token", AuthController.updateAccessToken);
-router.post("/logout", AuthController.logout);
-// router.get("/refresh-token/", AuthController.getPostById);
+router.get("/create-token", authMiddleware, AuthController.updateAccessToken);
+router.post("/logout", authMiddleware, AuthController.logout);
 
 module.exports = router;
